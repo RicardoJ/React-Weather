510efcad83fdbc990da83f3dcb9a367d255c3a1d
Ricardo Diaz <Ricardo.Diaz@endava.com>
2019-12-20 15:00:21 -0500

    adding form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import Header from './components/Header';
-
+import Form from './components/Form';
 function App() {
   return (
     <div >
       <Header
         titleHeader='Clima react app'
       />
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+            <div className="col s12 m6">
+              <Form />
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+function Form() {
+
+    const handleChange = e => {
+
+    }
+    return (
+        <form >
+            <div className="input-field col s12">
+                <input
+                    type="text"
+                    name="city"
+                    id="city"
+                    onChange={handleChange}
+                />
+                <label htmlFor="city">Ciudad: </label>
+            </div>
+            <div className="input-field col s12">
+                <select onChange={handleChange}
+                    name="country">
+                    <option value="">Selecciona un país</option>
+                    <option value="US">Estados Unidos</option>
+                    <option value="CO">Colombia</option>
+                    <option value="MX">México</option>
+                    <option value="AR">Argentina</option>
+                    <option value="CR">Costa Rica</option>
+                    <option value="ES">España</option>
+                    <option value="PE">Perú</option>
+                </select>
+            </div>
+            <div className="input-field col s12">
+                <input
+                    type="submit"
+                    className="waves-effect waves-light btn-large btn-block yellow accent-4"
+                    value="Buscar Clima" />
+            </div>
+
+        </form>
+    )
+
+}
+
+export default Form;
