5e25ac09e6aff784fc3b5a85c8ac5f8d6303a82f
Ricardo Diaz <Ricardo.Diaz@endava.com>
2019-12-23 14:11:00 -0500

    query function API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Form from './components/Form';
 import Error from './components/Error';
-function App() {
-  const [city, saveCity] = useState ('');
-  const [country , saveCountry] = useState('');
-  const [error, saveError] = useState(false);
 
-  const queryData = data  =>{
+function App() {
+  const [city, saveCity] = useState('');
+  const [country, saveCountry] = useState('');
+  const [error, saveError] = useState(false);
+  const [result, saveResult] = useState({});
+
+  useEffect(() => {
+    if (city === '') return;
+
+    const getAPI = async () => {
+
+      const appId = '4c2508f19313c213e6f9ab4a8f114471';
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${appId}`;
+
+      const response = await fetch(url);
+      const result = await response.json();
+
+      saveResult(result);
+
+    }
+    getAPI();
+  }, [city , country]);
+
+  const queryData = data => {
     if (data.city === '' || data.country === '') {
       saveError(true);
       return;
@@ -18,11 +37,12 @@ function App() {
     saveError(false);
   }
 
+
   //component conditionally
   let component;
   if (error) {
-    component = <Error message = 'Campos obligatorios' />
-  }else{
+    component = <Error message='Campos obligatorios' />
+  } else {
     component = null
   }
 
@@ -35,11 +55,11 @@ function App() {
         <div className="container">
           <div className="row">
             <div className="col s12 m6">
-              <Form 
-              queryData ={queryData}
+              <Form
+                queryData={queryData}
               />
             </div>
-            <div className = "col s12 m6">
+            <div className="col s12 m6">
               {component}
             </div>
           </div>
