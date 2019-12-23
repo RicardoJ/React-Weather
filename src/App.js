65bd903891f97569992c5df24c2fb2bf30610a8c
Ricardo Diaz <Ricardo.Diaz@endava.com>
2019-12-23 12:16:59 -0500

    showing error

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React, { useState } from 'react';
 import Header from './components/Header';
 import Form from './components/Form';
+import Error from './components/Error';
 function App() {
   const [city, saveCity] = useState ('');
   const [country , saveCountry] = useState('');
+  const [error, saveError] = useState(false);
 
   const queryData = data  =>{
     if (data.city === '' || data.country === '') {
-      
+      saveError(true);
       return;
     }
 
     saveCity(data.city);
     saveCountry(data.country);
+    saveError(false);
+  }
+
+  //component conditionally
+  let component;
+  if (error) {
+    component = <Error message = 'Campos obligatorios' />
+  }else{
+    component = null
   }
 
   return (
@@ -27,6 +38,9 @@ function App() {
               <Form 
               queryData ={queryData}
               />
+            </div>
+            <div className = "col s12 m6">
+              {component}
             </div>
           </div>
         </div>
